b33174d18df2e1f169850f1be613cafa940f06fe
Harrytsz <18810271846@163.com>
2019-11-01 17:28:44 +0800

    Create wordcloud_v0.py

---
+++ code/WordCloud/version0/wordcloud_v0.py
@@ -0,0 +1,17 @@
+# WordCloud - version 0.0 English Templates
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt 
+
+# Open text
+text = open('../constitution.txt').read()
+
+# Generate object
+wc = WordCloud().generate(text)
+
+# Display the wordcloud
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+# Save file
+wc.to_file('wc_v0_0.png')
