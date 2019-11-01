ef0eb0d8e2b1e5f434e5bbbe3a4ef7e4f51b562f
Harrytsz <18810271846@163.com>
2019-11-01 17:43:56 +0800

    Update wordcloud_v1.py

--- code/WordCloud/version1/wordcloud_v1.py
+++ code/WordCloud/version1/wordcloud_v1.py
@@ -1,1 +1,19 @@
 # WordCloud - version 1 English Templates
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt 
+
+# Open text
+text = open('../constitution.txt').read()
+
+# Generate object
+wc = WordCloud(font_path='../Hiragino.ttf', width=800, height=600, 
+	mode='RGBA', background_color=None).generate(text)
+
+# Display wordcloud
+plt.imshow(wc, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+# Save pic
+wc.to_file('wc_v1.png')
